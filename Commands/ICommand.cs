f1e1c71b5ce08fbc5da49a94e4a3258be6c83504
DanielKjr <93762921+DanielKjr@users.noreply.github.com>
2022-10-03 09:49:23 +0200

    Initial commit

---
+++ Commands/ICommand.cs
@@ -0,0 +1,7 @@
+﻿namespace BattleShips
+{
+    public interface ICommand
+    {
+        void Execute(Player player);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BattleShips
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = GameWorld.Instance)
+                game.Run();
+        }
+    }
+}
